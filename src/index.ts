59effaa2b6c7a0ccb000b0eb2d63d21851ac64ac
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-06-24 10:51:52 +0530

    routing hono

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  const body = await c.req.json()
-  return c.text('Hello Hono!')
+
+  return c.json({
+    name: "sarfraz ashrafi",
+    age: "21"
+  })
+})
+
+app.post("/user", async (c) => {
+  const title = await c.req.json()
+  console.log(title)
+  console.log(c.req.header("Authorization"))
+  console.log(c.req.method)
+  console.log(c.req.query("param"))
+  return c.json(title)
 })
 
 export default app
